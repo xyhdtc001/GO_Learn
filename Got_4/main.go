4af5500265ba3d404a96cb35b42cf1bc88ecc5f5
xyh123 <xyh123@qq.com>
2017-09-02 17:13:23 +0800

    updata

--- Got_4/main.go
+++ Got_4/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	array := [2]byte{1, 2}
-	zero(&array)
-	s := []int{0, 1, 2, 3, 4, 5}
-	reverse(s[:2])
-	fmt.Println(s)
-	reverse(s[2:])
-	fmt.Println(s)
-	reverse(s)
-	fmt.Println(s)
+
+	fmt.Println("argsLen", len(os.Args))
+	fmt.Println(os.Args)
+
+	_chart4_hashmapUse_()
+	//x, _ := strconv.ParseInt(os.Args[1], 10, 8)
+
+	//res := _switch_use(int(x))
+	//fmt.Printf("swtich x = %d : %d\n", x, res)
+
+	//array := [2]byte{1, 2}
+	//zero(&array)
+	//s := []int{0, 1, 2, 3, 4, 5}
+	//reverse(s[:2])
+	//fmt.Println(s)
+	//reverse(s[2:])
+	//fmt.Println(s)
+	//reverse(s)
+	//fmt.Println(s)
 }
 
 func zero(ptrArray *[2]byte) {
